eb2834e6c53a9b2862750f959f0d004989d34b72
Xxspatel2023xX <sarahpatel3@icloud.com>
2021-11-08 14:19:56 -0500

    Changes 11/8

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,20 +1,27 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {APP_SECRET, getUserId} = require('../utils');
+const { APP_SECRET, getUserId } = require('../utils');
 
-function post(parent, { url, description }, ctx, info) {
-  const userId = getUserId(ctx)
-  return ctx.db.mutation.createLink(
-    { data: { url, description, postedBy: { connect: { id: userId } } } },
-    info,
-  )
+function post(parent, args, context, info) {
+  const { userId } = context;
+
+  const newLink = context.prisma.link.create({
+    data: {
+      url: args.url,
+      description: args.description,
+      postedBy: { connect: { id: userId } }
+    }
+  });
+  context.pubsub.publish('NEW_LINK', newLink);
+
+  return newLink;
 }
+
 async function signup(parent, args, context, info) {
   const password = await bcrypt.hash(args.password, 10);
   const user = await context.prisma.user.create({
     data: { ...args, password }
   });
-
 
   const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
